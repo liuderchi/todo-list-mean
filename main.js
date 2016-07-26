80e322af17615eb56ce008b5af9bf2409006b4c0
liuderchi <liuderchi@gmail.com>
2016-07-26 11:52:31 +0800

    [feat] use sample data for demo without mongodb

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 'use babel'; // use babel transpiler
-require( './db-connect' ); // db connection
+// require( './db-connect' ); // db connection
 
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var mongoose = require( 'mongoose' );
-var Todo = mongoose.model( 'Todo' );
+// var mongoose = require( 'mongoose' );
+// var Todo = mongoose.model( 'Todo' );
 
 var util = require( './util' );
 
@@ -39,11 +39,14 @@ app.route(/^\/(home)/)
 
 app.route('/')
   .get((req, res) => {
-    Todo.find({})
-        .sort( '-updated_at' )
-        .exec( ( err, todos ) => {
-          res.status(200).send({tasks: todos});
-        });
+    // Todo.find({})
+    //     .sort( '-updated_at' )
+    //     .exec( ( err, todos ) => {
+    //       res.status(200).send({tasks: todos});
+    //     });
+
+    // NOTE use fakeData
+    return res.status(200).send(fakeData);
   })
   .post((req, res) => {
     // curl -X POST -H "Content-Type: application/json"  -d '{"task":"yes","status":true}' -w "\n"  http://localhost:3000/
@@ -51,13 +54,25 @@ app.route('/')
     _task = JSON.parse(JSON.stringify(req.body));
     _task.task_id = util.makeid();
 
-    new Todo({
+    if (fakeData.tasks.length == 30) {
+      return res.status(403).send({result:'failed', message:'reached limit of 30 tasks.'});
+    }
+
+    // new Todo({
+    //   task_id: _task.task_id,
+    //   task: _task.task,
+    //   status: false
+    // }).save((err, todo, count) => {
+    //     res.status(201).send({});
+    // });
+
+    // NOTE use fakeData
+    fakeData.tasks.push({
       task_id: _task.task_id,
       task: _task.task,
       status: false
-    }).save((err, todo, count) => {
-        res.status(201).send({});
-    });
+    })
+    return res.status(201).send({});
   });
 
 
@@ -67,40 +82,59 @@ app.route(/^\/(\w+)/)
     // curl -X PUT -H "Content-Type: application/json"  -d '{"task":"NewTask","status":true}' -w "\n"  http://localhost:3000/t_id
 
     var target_task_id = req.params[0]
-    var taskExist = false;
-    Todo.find({})
-        .exec(( err, todos ) =>{
-          for (task of todos) {
-            if (task.task_id === target_task_id){   // TODO use mongo filter
-              taskExist = true;
-              res.status(200).send({result: "ok"});
-              task.task = req.body.task;
-              task.status = req.body.status;
-              task.save(( err, todo, count ) => {
-                if( err ) console.log('error db update');
-              });
-            }
-          }
-          if (!taskExist) {  res.status(404).send('NOT FOUND');  }
-        });
+    // var taskExist = false;
+    // Todo.find({})
+    //     .exec(( err, todos ) =>{
+    //       for (task of todos) {
+    //         if (task.task_id === target_task_id){   // TODO use mongo filter
+    //           taskExist = true;
+    //           res.status(200).send({result: "ok"});
+    //           task.task = req.body.task;
+    //           task.status = req.body.status;
+    //           task.save(( err, todo, count ) => {
+    //             if( err ) console.log('error db update');
+    //           });
+    //         }
+    //       }
+    //       if (!taskExist) {  res.status(404).send('NOT FOUND');  }
+    //     });
+
+    // NOTE use fakeData
+    for (task of fakeData.tasks) {
+      if (task.task_id === target_task_id){
+        task.task = req.body.task;
+        task.status = req.body.status;
+        return res.status(200).send({result: "ok"});
+      }
+    }
+    return res.status(404).send('NOT FOUND');
   })
   .delete((req, res) => {
     // curl -X DELETE -H "Content-Type: application/json" -w "\n"  http://localhost:3000/t_id
 
     var target_task_id = req.params[0]
-    Todo.find({})
-        .exec(( err, todos ) =>{
-          for (task of todos) {
-            if (task.task_id === target_task_id){   // TODO use mongo filter
-              taskExist = true;
-              res.status(200).send({result: "ok"});
-              task.remove((err) => {
-                if( err ) console.log('error db update');
-              });
-            }
-          }
-          if (!taskExist) {  res.status(404).send('NOT FOUND');  }
-        });
+    // Todo.find({})
+    //     .exec(( err, todos ) =>{
+    //       for (task of todos) {
+    //         if (task.task_id === target_task_id){   // TODO use mongo filter
+    //           taskExist = true;
+    //           res.status(200).send({result: "ok"});
+    //           task.remove((err) => {
+    //             if( err ) console.log('error db update');
+    //           });
+    //         }
+    //       }
+    //       if (!taskExist) {  res.status(404).send('NOT FOUND');  }
+    //     });
+
+    // NOTE use fakeData
+    for (t in fakeData.tasks) {
+      if (fakeData.tasks[t].task_id === target_task_id){
+        fakeData.tasks.splice(t, 1);
+        return res.status(200).send({result: "ok"});
+      }
+    }
+    return res.status(404).send('NOT FOUND');
   });
 
 
