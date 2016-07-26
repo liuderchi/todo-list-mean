b2dc1dcf0ba0997dd5a8c6dee551345513d3436d
liuderchi <liuderchi@gmail.com>
2016-07-26 15:07:57 +0800

    [fix] check task number limit, refactor redundant code

--- main_heroku.js
+++ main_heroku.js
@@ -1,13 +1,9 @@
 'use babel'; // use babel transpiler
-// require( './db-connect' ); // db connection
 
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-
-// var mongoose = require( 'mongoose' );
-// var Todo = mongoose.model( 'Todo' );
 
 var util = require( './util' );
 
@@ -39,13 +35,6 @@ app.route(/^\/(home)/)
 
 app.route('/')
   .get((req, res) => {
-    // Todo.find({})
-    //     .sort( '-updated_at' )
-    //     .exec( ( err, todos ) => {
-    //       res.status(200).send({tasks: todos});
-    //     });
-
-    // NOTE use fakeData
     return res.status(200).send(fakeData);
   })
   .post((req, res) => {
@@ -54,17 +43,9 @@ app.route('/')
     _task = JSON.parse(JSON.stringify(req.body));
     _task.task_id = util.makeid();
 
-    if (fakeData.tasks.length == 30) {
+    if (fakeData.tasks.length >= 30) {
       return res.status(403).send({result:'failed', message:'reached limit of 30 tasks.'});
     }
-
-    // new Todo({
-    //   task_id: _task.task_id,
-    //   task: _task.task,
-    //   status: false
-    // }).save((err, todo, count) => {
-    //     res.status(201).send({});
-    // });
 
     // NOTE use fakeData
     fakeData.tasks.push({
@@ -76,28 +57,11 @@ app.route('/')
   });
 
 
-// app.route('/:task_id')   // console.log('delete a new task', req.params.task_id);
 app.route(/^\/(\w+)/)
   .put((req, res) => {
     // curl -X PUT -H "Content-Type: application/json"  -d '{"task":"NewTask","status":true}' -w "\n"  http://localhost:3000/t_id
 
     var target_task_id = req.params[0]
-    // var taskExist = false;
-    // Todo.find({})
-    //     .exec(( err, todos ) =>{
-    //       for (task of todos) {
-    //         if (task.task_id === target_task_id){   // TODO use mongo filter
-    //           taskExist = true;
-    //           res.status(200).send({result: "ok"});
-    //           task.task = req.body.task;
-    //           task.status = req.body.status;
-    //           task.save(( err, todo, count ) => {
-    //             if( err ) console.log('error db update');
-    //           });
-    //         }
-    //       }
-    //       if (!taskExist) {  res.status(404).send('NOT FOUND');  }
-    //     });
 
     // NOTE use fakeData
     for (task of fakeData.tasks) {
@@ -113,19 +77,6 @@ app.route(/^\/(\w+)/)
     // curl -X DELETE -H "Content-Type: application/json" -w "\n"  http://localhost:3000/t_id
 
     var target_task_id = req.params[0]
-    // Todo.find({})
-    //     .exec(( err, todos ) =>{
-    //       for (task of todos) {
-    //         if (task.task_id === target_task_id){   // TODO use mongo filter
-    //           taskExist = true;
-    //           res.status(200).send({result: "ok"});
-    //           task.remove((err) => {
-    //             if( err ) console.log('error db update');
-    //           });
-    //         }
-    //       }
-    //       if (!taskExist) {  res.status(404).send('NOT FOUND');  }
-    //     });
 
     // NOTE use fakeData
     for (t in fakeData.tasks) {
