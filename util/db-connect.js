a27616e0d264d7654a37cddd98a39eb3b69e81ac
liuderchi <liuderchi@gmail.com>
2016-07-28 10:28:29 +0800

    [feat] add mongo add-on of heroku
    - also support original local mongodb
    - update Procfile to run main.js on heroku, remove main_heroku.js

--- util/db-connect.js
+++ util/db-connect.js
@@ -8,4 +8,12 @@ var Todo = new Schema({
 });
 
 mongoose.model('Todo', Todo);
-mongoose.connect('mongodb://localhost/todo');
+
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/todo';
+// NOTE process.env.MONGODB_URI is for heroku env
+console.log('making db connect to ', mongoUri);
+
+mongoose.connect(mongoUri, (err) => {
+  if (err) console.error(err);
+  else console.log('mongo connected to:', mongoUri);
+});
