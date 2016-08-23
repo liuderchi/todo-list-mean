6abcadf9165f6c3be974b1f4230d135840c7bc51
liuderchi <liuderchi@gmail.com>
2016-08-23 14:31:18 +0800

    [test] POST/DELETE task specs

--- spec/server.js
+++ spec/server.js
@@ -14,7 +14,7 @@ describe("todo list RESTful api server", () => {
         url: base_url,
         timeout: 5000
       };
-      request.get(options, (error, response, body) => {
+      request.get(options, (error, response) => {
         expect(response.statusCode).toBe(200);
         done();
       });
@@ -28,7 +28,7 @@ describe("todo list RESTful api server", () => {
     describe("response data", () => {
       it("has field named tasks", (done) => {
         request.get(_url, (error, response, body) => {
-          expect(JSON.parse(response.body).tasks).toBeDefined();
+          expect(JSON.parse(body).tasks).toBeDefined();
           done();
         });
       });
@@ -36,7 +36,7 @@ describe("todo list RESTful api server", () => {
       describe("each task data", () => {
         it("has field named task_id, task, status", (done) => {
           request.get(_url, (error, response, body) => {
-            var _body = JSON.parse(response.body);
+            var _body = JSON.parse(body);
             if (_body.tasks.length > 0){
               var _task = _body.tasks[0];
               expect(_task.task_id).toBeDefined();
@@ -48,6 +48,49 @@ describe("todo list RESTful api server", () => {
         });
       });
 
+    });
+  });
+
+  describe("POST DELETE /todos", () => {
+
+    var _url = url.resolve(base_url, 'todos');
+    var task_count = 0;
+    var _task_id = '';
+
+    beforeEach((done) => {
+      request.get(_url, (error, response, body) => {
+        task_count = JSON.parse(body).tasks.length;
+        done();
+      });
+    });
+
+    it("creating a new todo then delete it would change task quantity", (done) => {
+      var data = {task: 'blablabla'};
+      var option = {
+        url: _url,
+        json: data,
+        timeout: 5000
+      };
+
+      request.post(option, (error, response) => {
+        expect(response.statusCode).toBe(201);
+        _task_id = response.body.task_id.toString();
+
+        request.get(_url, (error, response, body) => {
+          expect(JSON.parse(body).tasks.length).toBe(task_count + 1);
+
+          request.delete(
+          url.resolve(base_url, 'todos/' + _task_id), (error, response) => {
+            expect(response.statusCode).toBe(200);
+
+            request.get(_url, (error, response, body) => {
+              expect(JSON.parse(body).tasks.length).toBe(task_count);
+              done();
+
+            });
+          });
+        });
+      });
     });
   });
 
