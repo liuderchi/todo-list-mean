187bd7c27c15e9f60eb5f8442100e858034f9111
liuderchi <liuderchi@gmail.com>
2016-08-23 14:23:37 +0800

    [test] add spec of PUT method update task

--- spec/server.js
+++ spec/server.js
@@ -58,14 +58,15 @@ describe("todo list RESTful api server", () => {
     var task_count = 0;
     var _task_id = '';
 
-    beforeEach((done) => {
+    beforeAll((done) => {
+      // NOTE check current task quantity
       request.get(_url, (error, response, body) => {
         task_count = JSON.parse(body).tasks.length;
         done();
       });
     });
 
-    it("creating a new todo then delete it would change task quantity", (done) => {
+    it("creats a new todo and delete it would change task quantity", (done) => {
       if (task_count >= MAX_TASK_QTY) {
         fail('currently db reaches tasks limit: ' + MAX_TASK_QTY);
         done(); // NOTE leave current spec
@@ -97,6 +98,67 @@ describe("todo list RESTful api server", () => {
         });
       });
     });
+  });
+
+  describe("UPDATE /todos", () => {
+    var _url = url.resolve(base_url, 'todos');
+    var _task_id = '';
+    var task_count = 0;
+
+    beforeAll((done) => {
+      // NOTE checks current task quantity
+      // NOTE don't use it in before/after
+      request.get(_url, (error, response, body) => {
+        task_count = JSON.parse(body).tasks.length;
+        done();
+      });
+    });
+
+    it('create a new task', (done) => {
+      if (task_count >= MAX_TASK_QTY) {
+        // fail() should be used inside spec scope
+        fail('currently db reaches tasks limit: ' + MAX_TASK_QTY);
+        done(); // NOTE leave current spec
+      }
+      var data = {task: 'kerkerker'};
+      var option = {
+        url: _url,
+        json: data,
+        timeout: 5000
+      };
+      request.post(option, (error, response) => {
+        expect(response.statusCode).toBe(201);
+        _task_id = response.body.task_id.toString();
+
+        var json = {
+          task: 'foofoo',
+          status: true
+        };
+        var options = {
+          url: url.resolve(base_url, 'todos/' + _task_id),
+          json: json
+        };
+        request.put(options, (error, response) => {
+
+          expect(response.statusCode).toBe(200);
+          request.get(url.resolve(base_url, 'todos/' + _task_id), (error, response) => {
+            expect(JSON.parse(response.body).task).toBe('foofoo');
+            expect(JSON.parse(response.body).status).toBe(true);
+
+            done();
+          });
+        });
+      });
+    });
+
+    afterAll((done) => {
+      // NOTE delete created task
+      request.delete(url.resolve(base_url, 'todos/' + _task_id), (error, response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
   });
 
 });
