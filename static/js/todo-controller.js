91d9e73346fdca91445ae054925e8b34f8196dea
liuderchi <liuderchi@gmail.com>
2017-02-22 15:19:40 +0800

    [refactor] indentation

--- static/js/todo-controller.js
+++ static/js/todo-controller.js
@@ -1,8 +1,6 @@
 angular
 .module("todoController", ['modalController', 'ngAnimate', 'ui.bootstrap'])
-.controller("todoController", ['$scope', '$http', '$uibModal', '$log',
-($scope, $http, $uibModal, $log) => {
-    // NOTE $uibModal dependencies
+.controller("todoController", ['$scope', '$http', '$uibModal', '$log', ($scope, $http, $uibModal, $log) => {  // NOTE $uibModal dependencies
 
     $scope.tasksDone = [];
     $scope.tasksUndone = [];
@@ -21,21 +19,19 @@ angular
         var url = '/todos/';
         var data = {'task':taskName};
 
-        $http.post(url, data).then(
-            (resp) => {
-                $log.log('successPOST');
-                if (resp.status === 201){
-                    _sendGetUpdateAngModel();
-                    $scope.newTaskName = '';                   // clear input
-                    $scope.addAlert('created task: ' + taskName);
-                }
-            },
-            (resp) => {  // handle task limit condition
-                if (resp.status === 403){
-                    alert('reach 30 tasks limit');
-                }
-                $log.log('errorPOST');
-            });
+        $http.post(url, data).then((resp) => {
+            $log.log('successPOST');
+            if (resp.status === 201){
+                _sendGetUpdateAngModel();
+                $scope.newTaskName = '';                   // clear input
+                $scope.addAlert('created task: ' + taskName);
+            }
+        }, (resp) => {  // handle task limit condition
+            if (resp.status === 403){
+                alert('reach 30 tasks limit');
+            }
+            $log.log('errorPOST');
+        });
     };
 
     var _updateTaskAngModel = (resp) => {
@@ -55,14 +51,12 @@ angular
 
     var _sendGetUpdateAngModel = (url) => {
         url = url || '/todos/';   // get current host and port
-        $http.get(url).then(
-            (resp) => {
-                _updateTaskAngModel(resp);
-                $log.log('successGET');
-            },
-            () => {
-                $log.log('errorGET');
-            });
+        $http.get(url).then((resp) => {
+            _updateTaskAngModel(resp);
+            $log.log('successGET');
+        }, () => {
+            $log.log('errorGET');
+        });
     };
 
     // change task status
@@ -77,18 +71,16 @@ angular
         if(task.status !== 'undefined'){  data.status = task.status;  }
         if(task.task !== 'undefined'){  data.task = task.task;  }
 
-        $http.put(url, data).then(
-            (resp) => {
-                $log.log('successPUT');
-                if (resp.status === 200){
-                    _sendGetUpdateAngModel();
-                    $scope.addAlert('update success');
-                }
-            },
-            () => {
-                $log.log('errorPUT');
+        $http.put(url, data).then((resp) => {
+            $log.log('successPUT');
+            if (resp.status === 200){
                 _sendGetUpdateAngModel();
-            });
+                $scope.addAlert('update success');
+            }
+        },() => {
+            $log.log('errorPUT');
+            _sendGetUpdateAngModel();
+        });
     };
 
     $scope.onClickDeleteTask = (taskID, taskName) => {
@@ -111,17 +103,15 @@ angular
             // NOTE resolve function, modalResult passed from modal-controller
             $log.info('[Modal] ' + new Date() + ': ' + modalResult);
 
-            $http.delete(url).then(
-                (resp) => {
-                    $log.log('successDELETE');
-                    if (resp.status === 200){
-                        _sendGetUpdateAngModel();
-                        $scope.addAlert(resp.data.result + ' : ' + taskName );
-                    }
-                },
-                () => {
-                    $log.log('errorDELETE');
-                });
+            $http.delete(url).then((resp) => {
+                $log.log('successDELETE');
+                if (resp.status === 200){
+                    _sendGetUpdateAngModel();
+                    $scope.addAlert(resp.data.result + ' : ' + taskName );
+                }
+            }, () => {
+                $log.log('errorDELETE');
+            });
         }, (modalResultReject) => {
             // NOTE reject function, modalResultReject passed from modal-controller
             $log.info('[Modal] ' + new Date() + ': ' + modalResultReject);
